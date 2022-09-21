7226716f08d8651a1cc56417b503607dfecccc96
Muhammad Ismail <110536675+Muhammad-Ismail12@users.noreply.github.com>
2022-09-20 18:49:14 -0700

    Update vending_machine.sol

--- vending_machine.sol
+++ vending_machine.sol
@@ -1,34 +1,29 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+
 pragma solidity <0.9.0;
 
-contract VendingMachine {
+contract Vendingmachine{
 
-    // state variables
     address public owner;
-    mapping (address => uint) public donutBalances;
 
-    // set the owner as th address that deployed the contract
-    // set the initial vending machine balance to 100
-    constructor() {
+    constructor(){
         owner = msg.sender;
-        donutBalances[address(this)] = 100;
+        _donutstock[address(this)] = 200;
     }
 
-    function getVendingMachineBalance() public view returns (uint) {
-        return donutBalances[address(this)];
+    mapping(address => uint) public _donutstock;
+
+    function VendingMac_bal() public view returns(uint) {
+        return _donutstock[address(this)];
     }
 
-    // Let the owner restock the vending machine
-    function restock(uint amount) public {
-        require(msg.sender == owner, "Only the owner can restock.");
-        donutBalances[address(this)] += amount;
+    function purchase(uint _stock) public{
+        _donutstock[address(this)] -= _stock;
+        _donutstock[msg.sender] += _stock;
     }
 
-    // Purchase donuts from the vending machine
-    function purchase(uint amount) public payable {
-        require(msg.value >= amount * 2 ether, "You must pay at least 2 ETH per donut");
-        require(donutBalances[address(this)] >= amount, "Not enough donuts in stock to complete this purchase");
-        donutBalances[address(this)] -= amount;
-        donutBalances[msg.sender] += amount;
+    function restock(uint _stock) public{
+        require(msg.sender == owner , "Only owner can restock");
+        _donutstock[address(this)] += _stock;
     }
 }
