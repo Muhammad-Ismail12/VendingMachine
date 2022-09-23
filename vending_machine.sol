25eb72b5c5f1656794e345ddd9aa384ddf859532
Muhammad Ismail <110536675+Muhammad-Ismail12@users.noreply.github.com>
2022-09-23 11:50:36 -0700

    Update vending_machine.sol

--- vending_machine.sol
+++ vending_machine.sol
@@ -4,22 +4,34 @@ pragma solidity <0.9.0;
 
 contract Vendingmachine{
 
+    struct Purchaser{
+        address _purchaseraddress;
+        uint _quantity;
+        uint _purchasingtime;
+    }
+
     address public owner;
 
     constructor(){
-        owner = msg.sender;
+        owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
         _donutstock[address(this)] = 200;
     }
 
-    mapping(address => uint) public _donutstock;
+    mapping(address => Purchaser) public _details;
+    mapping(address => uint)  _donutstock;
 
     function VendingMac_bal() public view returns(uint) {
         return _donutstock[address(this)];
     }
 
-    function purchase(uint _stock) public{
-        _donutstock[address(this)] -= _stock;
-        _donutstock[msg.sender] += _stock;
+    function purchase(uint quantity) public payable{
+        _donutstock[address(this)] -= quantity;
+        _donutstock[msg.sender] += quantity;
+        _details[msg.sender] = Purchaser({
+            _purchaseraddress : msg.sender,
+            _quantity : quantity,
+            _purchasingtime : block.timestamp
+        });
     }
 
     function restock(uint _stock) public{
@@ -27,3 +39,4 @@ contract Vendingmachine{
         _donutstock[address(this)] += _stock;
     }
 }
+
